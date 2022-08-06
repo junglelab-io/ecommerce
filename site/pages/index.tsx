d92709f7324627217e1b5f83c74a880f0edea5ef
Esky33 <djescaba@icloud.com>
2022-08-06 16:21:01 +0100

    update config

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -59,8 +59,8 @@ export default function Home({
         ))}
       </Marquee>
       <Hero
-        headline=" Dessert dragée halvah croissant."
-        description="Cupcake ipsum dolor sit amet lemon drops pastry cotton candy. Sweet carrot cake macaroon bonbon croissant fruitcake jujubes macaroon oat cake. Soufflé bonbon caramels jelly beans. Tiramisu sweet roll cheesecake pie carrot cake. "
+        headline=" JUNGLELABᵀᴹ Swag."
+        description="JungleLab is an artist-driven Digital Assets Marketplace. Making it easy for digital creators to authenticate, showcase and sell the digital assets they produce on the Liquid Blockchain.. "
       />
       <Grid layout="B" variant="filled">
         {products.slice(0, 3).map((product: any, i: number) => (
